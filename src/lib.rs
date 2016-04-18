ad1c6b4ba49dc557786db8964f12866714a3267e
Albin Stjerna <albin.stjerna@gmail.com>
2016-04-18 15:31:18 +0200

    Implement printf etc.

--- src/lib.rs
+++ src/lib.rs
@@ -12,24 +12,8 @@ mod vga_buffer;
 
 #[no_mangle]
 pub extern fn rust_main() {
-    // let hello = b"Hello World!";
-    // let color_byte = 0x1f; // white foreground, blue background
-
-    // let mut hello_colored = [color_byte; 24];
-    // for(i, char_byte) in hello.into_iter().enumerate() {
-    //     hello_colored[i*2] = *char_byte;
-    // }
-
-    // // write to the center of the VGA text buffer -- n.b. that we are
-    // // using a memory-mapped write here (e.g. writing values to a
-    // // specific memory area).
-
-    // let buffer_ptr = (0xb8000 + 1988) as *mut _;
-    // unsafe { *buffer_ptr = hello_colored };
-    use core::fmt::Write;
-    vga_buffer::WRITER.lock().write_str("Hello again");
-    write!(vga_buffer::WRITER.lock(), ", some numbers: {} {}", 42, 1.337);
-
+    vga_buffer::clear_screen();
+    println!("Hello Rust!!{}", "!");
     loop{}
 }
 
--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -112,20 +112,26 @@ impl ::core::fmt::Write for Writer {
     }
 }
 
-pub fn print_something() {
-    let mut writer = Writer {
-        column_position: 24,
-        color_code: ColorCode::new(Color::LightGreen, Color::Black),
-        buffer: unsafe { Unique::new(0xb8000 as *mut _) },
-    };
-
-    writer.write_byte(b'H');
-    write!(writer, "The numbers are {} and {}", 42, 1.0/3.0);
-
-}
-
 pub static WRITER: Mutex<Writer> = Mutex::new(Writer {
     column_position: 0,
     color_code: ColorCode::new(Color::LightGreen, Color::Black),
     buffer: unsafe { Unique::new(0xb8000 as *mut _) },
 });
+
+macro_rules! println {
+    ($fmt:expr) => (print!(concat!($fmt, "\n")));
+    ($fmt:expr, $($arg:tt)*) => (print!(concat!($fmt, "\n"), $($arg)*));
+}
+
+macro_rules! print {
+    ($($arg:tt)*) => ({
+            use core::fmt::Write;
+            $crate::vga_buffer::WRITER.lock().write_fmt(format_args!($($arg)*)).unwrap();
+    });
+}
+
+pub fn clear_screen() {
+    for _ in 0..BUFFER_HEIGHT {
+        println!("");
+    }
+}
