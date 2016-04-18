49c179328ac8b89065c32457b778f72752e261ed
Albin Stjerna <albin.stjerna@gmail.com>
2016-04-18 15:12:52 +0200

    Implement as simple VGA buffer wrapper from phil-opp's tutorial.

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,34 @@
 #![feature(lang_items)]
 #![no_std]
 
+#![feature(const_fn)]
+#![feature(unique)]
+
 extern crate rlibc;
+extern crate spin;
+
+#[macro_use]
+mod vga_buffer;
 
 #[no_mangle]
 pub extern fn rust_main() {
-    let hello = b"Hello World!";
-    let color_byte = 0x1f; // white foreground, blue background
+    // let hello = b"Hello World!";
+    // let color_byte = 0x1f; // white foreground, blue background
 
-    let mut hello_colored = [color_byte; 24];
-    for(i, char_byte) in hello.into_iter().enumerate() {
-        hello_colored[i*2] = *char_byte;
-    }
+    // let mut hello_colored = [color_byte; 24];
+    // for(i, char_byte) in hello.into_iter().enumerate() {
+    //     hello_colored[i*2] = *char_byte;
+    // }
 
-    // write to the center of the VGA text buffer -- n.b. that we are
-    // using a memory-mapped write here (e.g. writing values to a
-    // specific memory area).
+    // // write to the center of the VGA text buffer -- n.b. that we are
+    // // using a memory-mapped write here (e.g. writing values to a
+    // // specific memory area).
 
-    let buffer_ptr = (0xb8000 + 1988) as *mut _;
-    unsafe { *buffer_ptr = hello_colored };
+    // let buffer_ptr = (0xb8000 + 1988) as *mut _;
+    // unsafe { *buffer_ptr = hello_colored };
+    use core::fmt::Write;
+    vga_buffer::WRITER.lock().write_str("Hello again");
+    write!(vga_buffer::WRITER.lock(), ", some numbers: {} {}", 42, 1.337);
 
     loop{}
 }
