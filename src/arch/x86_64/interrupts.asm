e790593b920e65758f0b59db1595bcec1a5f2a34
Albin Stjerna <albin.stjerna@gmail.com>
2016-04-20 16:16:20 +0200

    Template for an interrupt handler that saves the floating-point registers. It's not actually turned on just yet.

---
+++ src/arch/x86_64/interrupts.asm
@@ -0,0 +1,44 @@
+;;; This file contains interrupt handlers.
+
+extern rust_interrupt_handler
+
+section .text
+bits 64
+
+keyboard_interrupt_handler:
+        push rax
+        push rcx
+        push rdx
+        push r8
+        push r9
+        push r10
+        push r11
+        push rdi
+        push rsi
+
+        ;; Save floating-point registers
+        ;; Note: this is SLOW
+        fxsave [saved_floats]
+
+        call rust_interrupt_handler
+
+        ;; Restore floating-point registers
+        fxrstor [saved_floats]
+
+        pop rsi
+        pop rdi
+        pop r11
+        pop r10
+        pop r9
+        pop r8
+        pop rdx
+        pop rcx
+        pop rax
+
+        iretq
+
+segment .data
+        align 16
+saved_floats:
+        ;; Reserve space for FXSAVE
+        resb 512
