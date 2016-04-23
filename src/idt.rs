98c156159467cf97dc8cb23cc3f6cbad411d19ad
Albin Stjerna <albin.stjerna@gmail.com>
2016-04-23 15:56:46 +0200

    Deconstruct the IDT entries slightly more.

--- src/idt.rs
+++ src/idt.rs
@@ -1,7 +1,7 @@
 // This file contains functions related to reading and writing the IDT
 
 
-const IDT_NUM_ENTRIES: usize = 255;
+const NUM_INTERRUPTS: usize = 255;
 
 extern {
     fn _load_idt(x: u64);
@@ -16,14 +16,32 @@ pub unsafe fn idt_load(idt_pointer: u64) {
     _load_idt(idt_pointer);
 }
 
+/// An attribute flag segment for an IDT
+/// Stores: Present: true/false
+///         DPL (0-3 -- privilege level)
+///         Gate type
+#[derive(Clone, Copy)]
+struct IdtAttributes(u8);
 
+/// An entry in the IDT.
 #[repr(C, packed)]
 struct IdtEntry {
     base_low: u16,
     selector: u16,
     reserved_ist: u8, // 0
-    flags: u8,
+    attributes: IdtAttributes,
     base_mid: u16,
     base_high: u32,
     reserved_zero: u32,
+}
+
+#[repr(C, packed)]
+struct IdtTable {
+    table: [IdtEntry; NUM_INTERRUPTS],
+}
+
+impl IdtTable {
+    pub fn new() {
+
+    }
 }
