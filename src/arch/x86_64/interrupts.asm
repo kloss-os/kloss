b47352594dad04920c3a51cd337b245293a35eb4
Albin Stjerna <albin.stjerna@gmail.com>
2016-04-28 11:58:38 +0200

    Save all the registers!!!

--- src/arch/x86_64/interrupts.asm
+++ src/arch/x86_64/interrupts.asm
@@ -12,14 +12,20 @@ general_interrupt_handler:
         cli
 
         push rax
+        push rbx
         push rcx
         push rdx
         push r8
         push r9
         push r10
         push r11
+        push r12
+        push r13
+        push r14
+        push r15
         push rdi
         push rsi
+
 
         ;; Save floating-point registers
         ;; Note: this is SLOW
@@ -32,12 +38,17 @@ general_interrupt_handler:
 
         pop rsi
         pop rdi
+        pop r15
+        pop r14
+        pop r13
+        pop r12
         pop r11
         pop r10
         pop r9
         pop r8
         pop rdx
         pop rcx
+        pop rbx
         pop rax
 
         ;; FIXME: re-enable interrupts
