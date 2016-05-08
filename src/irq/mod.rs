d92a2f454134d1a9580a9d23ca6735430ebc0135
Albin Stjerna <albin.stjerna@gmail.com>
2016-05-08 16:22:29 +0200

    WIP: refactor interrupt-related code to its own module.

---
+++ src/irq/mod.rs
@@ -0,0 +1,6 @@
+//! This is a module to handle and set up the (software) side of
+//! interrupts. It (and its submodules) deals with setting up the IDT,
+//! wrapping assembler magic and so on.
+
+
+pub mod idt;
