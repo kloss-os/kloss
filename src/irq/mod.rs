5b7368c71ce9381e6007ce78f442cf4be5d34670
Albin Stjerna <albin.stjerna@gmail.com>
2016-05-09 10:25:05 +0200

    Move all wrapper functions to its own submodule.

--- src/irq/mod.rs
+++ src/irq/mod.rs
@@ -2,16 +2,28 @@
 //! interrupts. It (and its submodules) deals with setting up the IDT,
 //! wrapping assembler magic and so on.
 //! See also the x86 crate!
+//!
+//! # Usage
+//! Before doing anything else, you need to perform
+//! `irq::idt::install()` to configure the CPU to use the provided
+//! IDT. This will install the module's default ISR:s for every
+//! exception and interrupt. There is then two ways you can go about
+//! adding your own ISRs:
+//!
+//! 1. Add them using `irq::idt::set_gate()`. This will override the
+//!    system routine and the provided routine _must be an assembler
+//!    routine_ until Rust supports naked functions.
+//! 2. Add a Rust handler using `irq::set_handler()`. Note that this is
+//!    _way slower_ and more indirect than the direct CPU dispatch, as it
+//!    involves at least one layer of indirection between calls.
 
 
+// Modules and re-exports
 pub mod idt;
 
-// fixme: these should NOT BE PUBLIC
-extern {
-    pub fn general_interrupt_handler();
-    pub fn general_exception_handler();
-    pub fn null_interrupt_handler();
-    pub fn isr_42();
-    pub fn isr_255();
-    pub fn isr_12();
-}
+mod asm_wrappers;
+
+// FIXME: these should NOT BE PUBLIC
+pub use self::asm_wrappers::*;
+
+// End modules and re-exports
