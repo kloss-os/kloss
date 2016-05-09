da255af4f3bbdc7e0be95e83ea93b5f6ac60ba0d
Albin Stjerna <albin.stjerna@gmail.com>
2016-05-09 09:37:29 +0200

    WIP: Move externs to the inside of the irq module.

--- src/irq/mod.rs
+++ src/irq/mod.rs
@@ -1,6 +1,17 @@
 //! This is a module to handle and set up the (software) side of
 //! interrupts. It (and its submodules) deals with setting up the IDT,
 //! wrapping assembler magic and so on.
+//! See also the x86 crate!
 
 
 pub mod idt;
+
+// fixme: these should NOT BE PUBLIC
+extern {
+    pub fn general_interrupt_handler();
+    pub fn general_exception_handler();
+    pub fn null_interrupt_handler();
+    pub fn isr_42();
+    pub fn isr_255();
+    pub fn isr_12();
+}
