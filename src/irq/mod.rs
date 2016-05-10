85241f1f73f9b5e74a5f4689831262ad80339f94
Albin Stjerna <albin.stjerna@gmail.com>
2016-05-10 14:49:17 +0200

    New macro: `set_system_isr!`

--- src/irq/mod.rs
+++ src/irq/mod.rs
@@ -6,13 +6,15 @@
 //! # Usage
 //! Before doing anything else, you need to perform
 //! `irq::idt::install()` to configure the CPU to use the provided
-//! IDT. This will install the module's default ISRs (Interrupt Service
-//! Routines) for every exception and interrupt. There is then two ways
-//! you can go about adding your own ISRs:
+//! interrupt descriptor table (IDT). This will install the module's
+//! default ISRs (Interrupt Service Routines) for every exception and
+//! interrupt. There is then two ways you can go about adding your own
+//! ISRs:
 //!
 //! 1. Add them using `irq::idt::set_gate()`. This will override the
-//!    system routine and the provided routine _must be an assembler
-//!    routine_ until Rust supports naked functions.
+//!    system routine in the IDT and the provided routine _must be an
+//!    assembler routine_ following interrupt handler calling
+//!    conventions until Rust supports naked functions.
 //! 2. Add a Rust handler using `irq::set_handler()`. Note that this is
 //!    _way slower_ and more indirect than the direct CPU dispatch, as it
 //!    involves at least one layer of indirection between calls.
@@ -44,7 +46,7 @@ pub use self::dispatch::entry;
 /// 1. The module's IDT was installed using `irq::install()`, and
 /// 2. The ISR for `vec` has not been replaced with
 ///    `irq::idt::set_gate()`.
-/// If you are in doubt, use `set_system_isr()` to restore the system
+/// If you are in doubt, use `set_system_isr!()` to restore the system
 /// ISR dispatcher.
 ///
 /// - `vec` can be any integer in the range [0, 255].
@@ -58,7 +60,11 @@ pub fn set_handler(vec: u32,
 
 }
 
-/// Install/restore the system ISR for vector `vec`.
-pub fn set_system_isr(vec: u32) -> () {
-
+/// Install/restore the system ISR for a given vector.
+macro_rules! set_system_isr {
+    ($x:expr) => (irq::idt::set_gate($x, irq::general_exception_handler,
+                                     irq::idt::SELECT_TARGET_PRIV_1,
+                                     irq::idt::FLAG_TYPE_TRAP_GATE
+                                     | irq::idt::FLAG_DPL_KERNEL_MODE
+                                     | irq::idt::FLAG_GATE_ENABLED));
 }
