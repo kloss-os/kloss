3ed16dbe81a010804c6a9fc3f75c235809ea67a9
Albin Stjerna <albin.stjerna@gmail.com>
2016-05-13 11:22:30 +0200

    Add TESTS!!!!

--- src/irq/tests.rs
+++ src/irq/tests.rs
@@ -1,20 +1,25 @@
 #![cfg(test)]
 
-fn increment_counter() {
+static mut LAST_IRQ_CAUGHT: usize = 256;
 
+/// Just update the *gulp* global mutable variable
+unsafe fn log_interrupt(i: usize) {
+    LAST_IRQ_CAUGHT = i;
 }
 
 #[test]
 fn verify_dispatch_0() {
 
-    //super::entry(0); // Fake an interrupt
+    super::set_handler(0, log_interrupt);
+    super::entry(0); // Fake an interrupt
 
-    assert_eq!(4, 2 + 2);
+    unsafe{ assert_eq!(LAST_IRQ_CAUGHT, 0); }
 }
 
 #[test]
 fn verify_dispatch_255() {
-    //super::entry(255); // Fake an interrupt
+    super::set_handler(255, log_interrupt);
+    super::entry(255); // Fake an interrupt
 
-    assert_eq!(4, 2 + 2);
+    unsafe{ assert_eq!(LAST_IRQ_CAUGHT, 255); }
 }
